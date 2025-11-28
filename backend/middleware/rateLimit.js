8a20ca6b095fe3254acd1ce36d30adafd8530dc0
Rajdeep2302 <rajdeeppalofficial@gmail.com>
2025-11-28 15:10:43 +0530

    Fix express-rate-limit for production

--- backend/middleware/rateLimit.js
+++ backend/middleware/rateLimit.js
@@ -1,6 +1,6 @@
-import rateLimit from "express-rate-limit"
+import rateLimit from "express-rate-limit";
 
 export const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 200
-})
+  max: 200,
+});
